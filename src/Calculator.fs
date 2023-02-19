14fc93522451925ff4b44d06cfe9f2050b80c5cb
Kurt Mueller <kurtronaldmueller@gmail.com>
2023-02-19 11:31:34 -0500

    Move weight html to a type

--- src/Calculator.fs
+++ src/Calculator.fs
@@ -49,86 +49,91 @@ module Calculator =
                 MassUnit = MassUnit.Lb
             }, Cmd.none
 
-    let weightAmountLabel (inputHtmlId: string) : ReactElement =
-        Html.label [
-            prop.for' inputHtmlId
-            prop.className "form-label"
-            prop.text "Body Weight"
-        ]
+    type WeightHtml = {
+        Weight: Mass option
+        WeightUnit: MassUnit
+        Dispatch: Msg -> unit
+    } with
+        member private _.InputHtmlId = "form-control"
 
-    let weightAmountInput (dispatch: Msg -> unit) (inputHtmlId: string) (weight: Mass option) : ReactElement =
-        let baseProperties = [
-            prop.id inputHtmlId
-            prop.type' "number"
-            prop.className "form-control"
-            prop.placeholder "How much do you weigh?"
-            prop.onChange (fun updatedAmount -> dispatch (UpdateMassAmount updatedAmount))
-        ]
-
-        let valueProperty =
-            match weight with
-            | Some w -> [ prop.value w.AsFloat ]
-            | _ -> []
-
-        Html.input (baseProperties @ valueProperty)
-
-    let weightKgOption (dispatch: Msg -> unit) (massUnit: MassUnit) : ReactElement list =
-        let optionName = "kgUnitOption"
-
-        [
-            Html.input [
-                prop.type' "radio"
-                prop.className "btn-check"
-                prop.name "weightUnitOptions"
-                prop.id optionName
-                prop.isChecked massUnit.IsKilogram
-                prop.onClick (fun _ -> dispatch ConvertAmountToKg)
-            ]
+        member this.Label : ReactElement =
             Html.label [
-                prop.classes ["btn"; "btn-secondary"]
-                prop.for' optionName
-                prop.text "Kg"
+                prop.for' this.InputHtmlId
+                prop.className "form-label"
+                prop.text "Body Weight"
             ]
-        ]
 
-    let weightLbOption (dispatch: Msg -> unit) (massUnit: MassUnit) : ReactElement list =
-        let optionName = "lbUnitOption"
-
-        [
-            Html.input [
-                prop.type' "radio"
-                prop.className "btn-check"
-                prop.name "weightUnitOptions"
-                prop.id optionName
-                prop.isChecked massUnit.IsPound
-                prop.onClick (fun _ -> dispatch ConvertAmountToLb)
+        member this.Input : ReactElement =
+            let baseProperties = [
+                prop.id this.InputHtmlId
+                prop.type' "number"
+                prop.className "form-control"
+                prop.placeholder "How much do you weigh?"
+                prop.onChange (fun updatedAmount -> this.Dispatch (UpdateMassAmount updatedAmount))
             ]
-            Html.label [
-                prop.classes ["btn"; "btn-secondary"]
-                prop.for' optionName
-                prop.text "Lb"
-            ]
-        ]
 
-    let weightAmountHtml (dispatch: Msg -> unit) (weight: Mass option) (massUnit: MassUnit ): ReactElement =
-        let inputId = "mass-amount-input"
+            let valueProperty =
+                match this.Weight with
+                | Some w -> [ prop.value w.AsFloat ]
+                | _ -> []
 
-        let inputHtmls =
-            [ weightAmountInput dispatch inputId weight ]
-            @ weightLbOption dispatch massUnit
-            @ weightKgOption dispatch massUnit
+            Html.input (baseProperties @ valueProperty)
 
-        Html.div [
-            prop.className "mb-3"
-            prop.children [
-                weightAmountLabel inputId
+        member this.WeightKgOption : ReactElement list =
+            let optionName = "kgUnitOption"
 
-                Html.div [
-                    prop.className "input-group"
-                    prop.children inputHtmls
+            [
+                Html.input [
+                    prop.type' "radio"
+                    prop.className "btn-check"
+                    prop.name "weightUnitOptions"
+                    prop.id optionName
+                    prop.isChecked this.WeightUnit.IsKilogram
+                    prop.onClick (fun _ -> this.Dispatch ConvertAmountToKg)
+                ]
+                Html.label [
+                    prop.classes ["btn"; "btn-secondary"]
+                    prop.for' optionName
+                    prop.text "Kg"
                 ]
             ]
-        ]
+
+        member this.WeightLbOption : ReactElement list =
+            let optionName = "lbUnitOption"
+
+            [
+                Html.input [
+                    prop.type' "radio"
+                    prop.className "btn-check"
+                    prop.name "weightUnitOptions"
+                    prop.id optionName
+                    prop.isChecked this.WeightUnit.IsPound
+                    prop.onClick (fun _ -> this.Dispatch ConvertAmountToLb)
+                ]
+                Html.label [
+                    prop.classes ["btn"; "btn-secondary"]
+                    prop.for' optionName
+                    prop.text "Lb"
+                ]
+            ]
+
+        member this.View : ReactElement =
+            let inputHtmls =
+                [ this.Input ]
+                @ this.WeightKgOption
+                @ this.WeightLbOption
+
+            Html.div [
+                prop.className "mb-3"
+                prop.children [
+                    this.Label
+
+                    Html.div [
+                        prop.className "input-group"
+                        prop.children inputHtmls
+                    ]
+                ]
+            ]
 
     [<ReactComponent>]
     let View() : ReactElement =
@@ -139,15 +144,18 @@ module Calculator =
             | Some mass -> Html.h1 mass.AsFloat
             | None -> Html.h1 "no amount"
 
-        let weightAmountHtml =
-            weightAmountHtml dispatch state.Weight state.MassUnit
+        let weightHtml = {
+            Weight =  state.Weight
+            WeightUnit = state.MassUnit
+            Dispatch = dispatch
+        }
 
         fluidContainer [
             row [
                 col [
                     massAmountH1 state.Weight
 
-                    weightAmountHtml
+                    weightHtml.View
                 ]
             ]
         ]
