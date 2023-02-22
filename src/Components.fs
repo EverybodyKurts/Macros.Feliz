1596d203a4847e490ece8e5c8a9b4d632981c0ee
Kurt Mueller <kurtronaldmueller@gmail.com>
2023-02-22 10:06:40 -0500

    Remove demo components

--- src/Components.fs
+++ src/Components.fs
@@ -5,27 +5,6 @@ open Feliz.Router
 
 type Components =
     /// <summary>
-    /// The simplest possible React component.
-    /// Shows a header with the text Hello World
-    /// </summary>
-    [<ReactComponent>]
-    static member HelloWorld() = Html.h1 "Hello World"
-
-    /// <summary>
-    /// A stateful React component that maintains a counter
-    /// </summary>
-    [<ReactComponent>]
-    static member Counter() =
-        let (count, setCount) = React.useState(0)
-        Html.div [
-            Html.h1 count
-            Html.button [
-                prop.onClick (fun _ -> setCount(count + 1))
-                prop.text "Increment"
-            ]
-        ]
-
-    /// <summary>
     /// A React component that uses Feliz.Router
     /// to determine what to show based on the current URL
     /// </summary>
@@ -37,10 +16,6 @@ type Components =
             router.children [
                 match currentUrl with
                 | [ ] -> Html.h1 "Index"
-                | [ "hello" ] -> Components.HelloWorld()
-                | [ "counter" ] -> Components.Counter()
-                | [ "elmish" ] -> Elmish.Counter()
-                | [ "macros" ] -> Calculator.View()
                 | otherwise -> Html.h1 "Not found"
             ]
         ]
