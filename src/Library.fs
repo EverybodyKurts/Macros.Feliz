bd70a9c69510ca7b061e4d61bb3f90d1dd213b3b
Kurt Mueller <kurtronaldmueller@gmail.com>
2023-09-22 00:19:40 -0400

    Remove unused method

--- src/Library.fs
+++ src/Library.fs
@@ -190,16 +190,6 @@ module Library =
                 | ``Lightly Active`` -> 1.55
                 | ``Highly Active`` -> 1.75
 
-            member this.Text : string =
-                match this with
-                | Sedentary -> "Sedentary"
-                | ``Mostly Sedentary`` -> "Mostly Sedentary"
-                | ``Lightly Active`` -> "Lightly Active"
-                | ``Highly Active`` -> "Highly Active"
-
-            static member Default =
-                Sedentary
-
         /// Holds the daily macronutrient breakdown for protein, carb, and fat.
         type DailyMacronutrient = internal {
             Grams: float<g>
