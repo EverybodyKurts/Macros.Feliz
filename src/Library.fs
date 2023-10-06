787ead896104959d52709c6b9ce5684c2330caa0
Kurt Mueller <kurtronaldmueller@gmail.com>
2023-10-06 15:48:09 -0400

    Uncomment out used code

--- src/Library.fs
+++ src/Library.fs
@@ -361,76 +361,76 @@ module Library =
                 | Some _, _ -> Error "Weight amount must be >= 0"
                 | None, _ -> Error "Weight amount must be present"
 
-            // /// Convert body weight to kg.
-            // /// - If the body weight is a valid amount, it will be converted to kg.
-            // /// - If the body weight is not a valid amount, the unit will be set to kg.
-            // member this.ToKg () : Weight =
-            //     match this.Validate () with
-            //     | Ok weight -> weight.ToKg () |> Weight.Create
-            //     | Error _ -> { this with Unit = Kg }
+            /// Convert body weight to kg.
+            /// - If the body weight is a valid amount, it will be converted to kg.
+            /// - If the body weight is not a valid amount, the unit will be set to kg.
+            member this.ToKg () : Weight =
+                match this.Validate () with
+                | Ok weight -> weight.ToKg () |> Weight.Create
+                | Error _ -> { this with Unit = Kg }
 
-            // /// Convert bodyweight to lb.
-            // /// - If the body weight is a valid amount, it will be converted to lb.
-            // /// - If the body weight is not a valid amount, the unit will be set to lb.
-            // member this.ToLb () : Weight =
-            //     match this.Validate () with
-            //     | Ok weight -> weight.ToLb () |> Weight.Create
-            //     | Error _ -> { this with Unit = Lb }
+            /// Convert bodyweight to lb.
+            /// - If the body weight is a valid amount, it will be converted to lb.
+            /// - If the body weight is not a valid amount, the unit will be set to lb.
+            member this.ToLb () : Weight =
+                match this.Validate () with
+                | Ok weight -> weight.ToLb () |> Weight.Create
+                | Error _ -> { this with Unit = Lb }
 
         type BodyComposition = {
             Weight: Weight
             BodyfatPercentage: int option
-        }
-        //     static member Default : BodyComposition =
-        //         {
-        //             Weight = Weight.Default
-        //             BodyfatPercentage = None
-        //         }
+        } with
+            static member Default : BodyComposition =
+                {
+                    Weight = Weight.Default
+                    BodyfatPercentage = None
+                }
 
-        //     static member Create (bodyComposition: Domain.BodyComposition) : BodyComposition =
-        //         {
-        //             Weight = Weight.Create bodyComposition.BodyWeight
-        //             BodyfatPercentage = Some <| int bodyComposition.BodyfatPercentage
-        //         }
+            static member Create (bodyComposition: Domain.BodyComposition) : BodyComposition =
+                {
+                    Weight = Weight.Create bodyComposition.BodyWeight
+                    BodyfatPercentage = Some <| int bodyComposition.BodyfatPercentage
+                }
 
-        //     member private this.ValidatePercentage() : Result<uint<pct>, string> =
-        //         match this.BodyfatPercentage with
-        //         | Some bfPct when 0 <= bfPct && bfPct <= 100 ->
-        //             Ok <| (uint bfPct) * 1u<pct>
-        //         | Some _ -> Error "Bodyfat % must be betwen 0 and 100"
-        //         | None -> Error "Bodyfat % must be present"
+            member private this.ValidatePercentage() : Result<uint<pct>, string> =
+                match this.BodyfatPercentage with
+                | Some bfPct when 0 <= bfPct && bfPct <= 100 ->
+                    Ok <| (uint bfPct) * 1u<pct>
+                | Some _ -> Error "Bodyfat % must be betwen 0 and 100"
+                | None -> Error "Bodyfat % must be present"
 
-        //     member this.Validate() : Validation<Domain.BodyComposition, string> =
-        //         validation {
-        //             let! weight = this.Weight.Validate()
-        //             and! bfPct = this.ValidatePercentage()
+            member this.Validate() : Validation<Domain.BodyComposition, string> =
+                validation {
+                    let! weight = this.Weight.Validate()
+                    and! bfPct = this.ValidatePercentage()
 
-        //             return {
-        //                 BodyWeight = weight
-        //                 BodyfatPercentage = bfPct
-        //             }
-        //         }
+                    return {
+                        BodyWeight = weight
+                        BodyfatPercentage = bfPct
+                    }
+                }
 
-        //     member this.ToKg() : BodyComposition =
-        //         { this with
-        //             Weight = this.Weight.ToKg()
-        //         }
+            member this.ToKg() : BodyComposition =
+                { this with
+                    Weight = this.Weight.ToKg()
+                }
 
-        //     member this.ToLb() : BodyComposition =
-        //         { this with
-        //             Weight = this.Weight.ToLb()
-        //         }
+            member this.ToLb() : BodyComposition =
+                { this with
+                    Weight = this.Weight.ToLb()
+                }
 
-        //     member this.UpdateWeightAmount (amount: float) : BodyComposition =
-        //         let weight = {
-        //             this.Weight with
-        //                 Amount = Some amount
-        //         }
+            member this.UpdateWeightAmount (amount: float) : BodyComposition =
+                let weight = {
+                    this.Weight with
+                        Amount = Some amount
+                }
 
-        //         { this with Weight = weight }
+                { this with Weight = weight }
 
-        //     member this.UpdateBodyfatPercentage (percentage: int) : BodyComposition =
-        //         { this with BodyfatPercentage = Some percentage }
+            member this.UpdateBodyfatPercentage (percentage: int) : BodyComposition =
+                { this with BodyfatPercentage = Some percentage }
 
         module DailyActivityLevel =
             let tryCreate (input: string) : Domain.DailyActivityLevel option =
